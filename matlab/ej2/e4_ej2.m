ab2ddca488fdbe1036ae19dc8b77371821247aa9
rocioparra <rparra@itba.edu.ar>
2020-04-02 23:13:55 -0300

    irr plot, some progress on ex2 report

--- matlab/ej2/e4_ej2.m
+++ matlab/ej2/e4_ej2.m
@@ -21,15 +21,16 @@ Toff = Ts-Ton;
 delta_iL = (Vdf+Vo*(1+rL/R))*Toff/L;
 
 didt = 50e+6;
+Irr = 1.7;
+trr = 75e-9;
+
 t3 = (Io-delta_iL/2)/didt;
-t4 = 34e-9;
-trr = 50e-9;
-t5 = trr -t4;
-Irr = didt *t4;
+t4 = Irr/didt;
+t5 = trr-t4;
 
 
 N = 50000;
-Tf = 3*Ts;
+Tf = 1.1*Ts;
 t = linspace(0, Tf, N);
 disparo = t;
 iL = t;
@@ -65,36 +66,44 @@ vl_spice = data(:,2);
 id_spice = data(:,3);
 il_spice = data(:,4);
 
-ax1 = subplot(4,1,1);
-plot(t, disparo);
-ylim([-0.5, 1.5]);
-xlim([0, Tf]);
-xticklabels({});
-formataxes('', '', 'Nivel l\''ogico');
+% ax1 = subplot(4,1,1);
+% plot(t, disparo);
+% ylim([-0.5, 1.5]);
+% xlim([0, Tf]);
+% xticklabels({});
+% formataxes('', '', 'Nivel l\''ogico');
+% 
+% ax2 = subplot(4,1,2);
+% plot(t, vL);
+% hold on;
+% plot(t_spice, vl_spice);
+% xlim([0, Tf]);
+% xticklabels({});
+% formataxes('', '', '$v_L(t)$ (V)', 'C\''alculo', 'Simulaci\''on');
+% 
+% ax3 = subplot(4,1,3);
+% plot(t, iL);
+% hold on;
+% plot(t_spice, il_spice);
+% xlim([0, Tf]);
+% xticklabels({});
+% formataxes('', '', '$i_L(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
+% 
+% 
+% ax4 = subplot(4,1,4);
+% plot(t, id);
+% hold on;
+% plot(t_spice, id_spice);
+% xlim([0, Tf]);
+% ylim([-2, 0.8]);
+% formataxes('', 'Tiempo (s)', '$i_D(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
 
-ax2 = subplot(4,1,2);
-plot(t, vL);
-hold on;
-plot(t_spice, vl_spice);
-xlim([0, Tf]);
-xticklabels({});
-formataxes('', '', '$v_L(t)$ (V)', 'C\''alculo', 'Simulaci\''on');
-
-ax3 = subplot(4,1,3);
-plot(t, iL);
-hold on;
-plot(t_spice, il_spice);
-xlim([0, Tf]);
-xticklabels({});
-formataxes('', '', '$i_L(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
-
-
-ax4 = subplot(4,1,4);
 plot(t, id);
 hold on;
 plot(t_spice, id_spice);
 xlim([0, Tf]);
 ylim([-2, 0.8]);
 formataxes('', 'Tiempo (s)', '$i_D(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
+
 
 
