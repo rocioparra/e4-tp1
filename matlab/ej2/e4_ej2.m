3061e0142c42ba8ef0b5dde41fd22ce5847724d4
rocioparra <rparra@itba.edu.ar>
2020-04-03 00:19:52 -0300

    ex2: v0 complete

--- matlab/ej2/e4_ej2.m
+++ matlab/ej2/e4_ej2.m
@@ -1,3 +1,6 @@
+clear;
+clear vars;
+
 fs = 50e3;
 Ts = 1/fs;
 
@@ -30,7 +33,7 @@ t5 = trr-t4;
 
 
 N = 50000;
-Tf = 1.1*Ts;
+Tf = 3*Ts;
 t = linspace(0, Tf, N);
 disparo = t;
 iL = t;
@@ -66,38 +69,32 @@ vl_spice = data(:,2);
 id_spice = data(:,3);
 il_spice = data(:,4);
 
-% ax1 = subplot(4,1,1);
-% plot(t, disparo);
-% ylim([-0.5, 1.5]);
-% xlim([0, Tf]);
-% xticklabels({});
-% formataxes('', '', 'Nivel l\''ogico');
-% 
-% ax2 = subplot(4,1,2);
-% plot(t, vL);
-% hold on;
-% plot(t_spice, vl_spice);
-% xlim([0, Tf]);
-% xticklabels({});
-% formataxes('', '', '$v_L(t)$ (V)', 'C\''alculo', 'Simulaci\''on');
-% 
-% ax3 = subplot(4,1,3);
-% plot(t, iL);
-% hold on;
-% plot(t_spice, il_spice);
-% xlim([0, Tf]);
-% xticklabels({});
-% formataxes('', '', '$i_L(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
-% 
-% 
-% ax4 = subplot(4,1,4);
-% plot(t, id);
-% hold on;
-% plot(t_spice, id_spice);
-% xlim([0, Tf]);
-% ylim([-2, 0.8]);
-% formataxes('', 'Tiempo (s)', '$i_D(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
+ax1 = subplot(4,1,1);
+plot(t, disparo);
+ylim([-0.5, 1.5]);
+xlim([0, Tf]);
+xticklabels({});
+formataxes('', '', 'Nivel l\''ogico');
 
+ax2 = subplot(4,1,2);
+plot(t, vL);
+hold on;
+plot(t_spice, vl_spice);
+xlim([0, Tf]);
+ylim([-7, 7]);
+xticklabels({});
+formataxes('', '', '$v_L(t)$ (V)', 'C\''alculo', 'Simulaci\''on');
+
+ax3 = subplot(4,1,3);
+plot(t, iL);
+hold on;
+plot(t_spice, il_spice);
+xlim([0, Tf]);
+xticklabels({});
+formataxes('', '', '$i_L(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
+
+
+ax4 = subplot(4,1,4);
 plot(t, id);
 hold on;
 plot(t_spice, id_spice);
@@ -105,5 +102,12 @@ xlim([0, Tf]);
 ylim([-2, 0.8]);
 formataxes('', 'Tiempo (s)', '$i_D(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
 
+% plot(t, id);
+% hold on;
+% plot(t_spice, id_spice);
+% xlim([0, Tf]);
+% ylim([-2, 0.8]);
+% formataxes('', 'Tiempo (s)', '$i_D(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
+% 
 
 
