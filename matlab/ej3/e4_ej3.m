8604aa9af5b065608180c62284e22501c13b4461
rocioparra <rparra@itba.edu.ar>
2020-04-03 03:01:48 -0300

    ex3: comp with ex2 graphs

--- matlab/ej3/e4_ej3.m
+++ matlab/ej3/e4_ej3.m
@@ -1,5 +1,5 @@
-clear;
-clear vars;
+clearvars;
+close all;
 
 fs = 50e3;
 Ts = 1/fs;
@@ -22,7 +22,6 @@ Ton = D*Ts;
 Toff = Ts-Ton;
 
 delta_iL = (Vdf+Vo*(1+rL/R))*Toff/L;
-
 
 didt = 50e+6;
 Irr = 1.7;
@@ -64,51 +63,66 @@ for i=1:N
 end
 
 data = csvread('e4_tp1_ej2.csv', 1, 0);
-t_spice = data(:,1);
-vl_spice = data(:,2);
-id_spice = data(:,3);
-il_spice = data(:,4);
-
-for i=1:size(vl_spice, 1)
-    vl_spice(i) = vl_spice(i) - il_spice(i)*rL;
+t_2 = data(:,1);
+vl_2 = data(:,2);
+id_2 = data(:,3);
+il_2 = data(:,4);
+for i=1:size(t_2, 1)
+    vl_2(i) = vl_2(i) - il_2(i)*rL;
 end
+disparo_2 = -(disparo-1)*10;
+
+
+data = csvread('e4_tp1_ej3.csv', 1, 0);
+t_3 = data(:,1);
+disparo_3 = data(:,3);
+vl_3 = data(:,2);
+id_3 = data(:,4);
+il_3 = data(:,5);
+
+
 
 ax1 = subplot(4,1,1);
-plot(t, disparo);
-ylim([-0.5, 1.5]);
+plot(t, disparo_2);
+hold on;
+plot(t_3, disparo_3);
+ylim([-1.5, 11.5]);
 xlim([0, Tf]);
 xticklabels({});
-formataxes('', '', 'Nivel l\''ogico');
+formataxes('', '', '$v_s(t)$ V', 'Llave ideal', 'Llave real');
 
 ax2 = subplot(4,1,2);
-plot(t, vL);
+plot(t_2, vl_2);
 hold on;
-plot(t_spice, vl_spice);
+plot(t_3, vl_3);
 xlim([0, Tf]);
 ylim([-7, 7]);
 xticklabels({});
-formataxes('', '', '$v_L(t)$ (V)', 'C\''alculo', 'Simulaci\''on');
+formataxes('', '', '$v_L(t)$ (V)');
 
 ax3 = subplot(4,1,3);
-plot(t, iL);
+% plot(t, iL);
+% hold on;
+plot(t_2, il_2);
 hold on;
-plot(t_spice, il_spice);
+plot(t_3, il_3);
 xlim([0, Tf]);
+ylim([0.21, 0.55]);
 xticklabels({});
-formataxes('', '', '$i_L(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
+formataxes('', '', '$i_L(t)$ (A)');
 
 
 ax4 = subplot(4,1,4);
-plot(t, id);
+plot(t_2, id_2);
 hold on;
-plot(t_spice, id_spice);
+plot(t_3, id_3);
 xlim([0, Tf]);
 ylim([-2, 0.8]);
-formataxes('', 'Tiempo (s)', '$i_D(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
+formataxes('', 'Tiempo (s)', '$i_D(t)$ (A)');
 
 % plot(t, id);
 % hold on;
-% plot(t_spice, id_spice);
+% plot(t_2, id_2);
 % xlim([0, Tf]);
 % ylim([-2, 0.8]);
 % formataxes('', 'Tiempo (s)', '$i_D(t)$ (A)', 'C\''alculo', 'Simulaci\''on');
